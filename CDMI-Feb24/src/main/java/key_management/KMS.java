9ea1c39d7674188ed95d38bf073423cdb9551bdd
Wang <wangqiyanhust@163.com>
2016-08-12 09:22:54 +0800

    finish the KMS and Authorization, update applicationContext.xml

--- CDMI-Feb24/src/main/java/key_management/KMS.java
+++ CDMI-Feb24/src/main/java/key_management/KMS.java
@@ -6,6 +6,10 @@ import net.minidev.json.JSONArray;
 import net.minidev.json.JSONStyle;
 import net.minidev.json.parser.ParseException;
 import java.io.*;
+import java.util.UUID;
+import net.minidev.json.parser.JSONParser;
+import org.snia.cdmiserver.util.RandomStringUtils;
+import org.snia.cdmiserver.model.DataObject;
 
 /**
  *
@@ -14,91 +18,118 @@ import java.io.*;
 public class KMS {
 
     private String filePath = "C:/data/key.txt";
-    
-    private KeySet keySet ;
 
-    public KeySet getKeySet() {
-        return keySet;
+    private Authorization auth ;
+
+    public Authorization getKeySet() {
+        return auth;
     }
 
-    public void setKeySet(KeySet keySet) {
-        this.keySet = keySet;
+    public void setKeySet(Authorization auth) {
+        this.auth = auth;
     }
 
     public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
-
-
 //    public static void main(String[] args) throws Exception {
 //        KMS kms = new KMS();
-//
+//        Authorization auth = new Authorization();
 //        // Create some default usernames and keys 
-//        kms.createKey("Kevin", "asdfghjk");
-//        kms.createKey("Sam", "aesfghjk");
-//        kms.createKey("Lilei", "qwefghjk");
-//        kms.createKey("Zhaokang", "xcvfghjk");
-//        kms.createKey("Guohuan", "atyuiojk");
-//        kms.createKey("Zhangsan", "aghjkhjk");
-//        kms.createKey("Wanger", "mnbfghjk");
-//        kms.createKey("Hanmeimei", "oiufghjk");
-//
-//        kms.createKey("", "");
-//        kms.getKey("Kevin");
-//        kms.getKey("Guohuan");
-//
-//        System.out.println(kms.getKey("Guohuan"));
+//        DataObject dObj = new DataObject();
+//        dObj.setObjectID("stupid");
+//        //kms.createKey(auth, "flt", dObj);
+//        System.out.println("the keyValue = " + kms.getKey("flt", dObj));
 //    }
 
-    public String getKey(String userName) throws Exception {
+    public String getKey(String authName, DataObject dObj) throws Exception {
         try {
             File f = new File(filePath);
             InputStreamReader isr = new InputStreamReader(new FileInputStream(f));
             BufferedReader br = new BufferedReader(isr);
             String line = br.readLine();
+
             while (line != null) {
                 Object obj = JSONValue.parse(line);
                 JSONObject jobj = (JSONObject) obj;
-                if (jobj.containsKey(userName)) {
-                    System.out.println(jobj.toJSONString());
-                    JSONObject temp = (JSONObject) (jobj.get(userName));
-
-                    return temp.get("key").toString();
+                if (jobj.containsKey(authName)) {
+                    JSONArray jarray = (JSONArray) jobj.get(authName);
+                    for (int i = 0; i < jarray.size(); i++) {
+                        JSONParser jp = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
+                        Authorization tmp = jp.parse(jarray.get(i).toString(), Authorization.class);
+                        if (tmp.getObjId().equals(dObj.getObjectID())) {
+                            return tmp.getKeyValue();
+                        }
+                    }
+                    return "no such object.";
                 }
                 line = br.readLine();
             }
         } catch (Exception e) {
+
         }
 
-        return "Did not find the user.";
+        return null;
     }
 
-    public boolean createKey(String userName, String key) {
-        if (userName == null) {
-            System.out.println("Null username!");
+    public StringBuilder getContent() throws Exception {
+        File f = new File(filePath);
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(f));
+        BufferedReader br = new BufferedReader(isr);
+        String line = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        while (line != null) {
+            sb.append(line);
+            line = br.readLine();
+        }
+        return sb;
+    }
+
+    public boolean createKey(String authName, DataObject dObj) throws Exception {
+        if (auth == null) {
+            System.out.println("Null auth!");
             return false;
         }
-        if (key.getBytes().length != 8) {
-            System.out.println("Key length wrong! 32 bytes string required!");
-            return false;
+        //JSONObject tmp = new JSONObject();
+        StringBuilder sb = this.getContent();
+        Object obj = JSONValue.parse(sb.toString());
+
+        JSONObject jcontent = (JSONObject) obj;
+        if (jcontent.get(authName) != null) {
+            if (this.getKey(authName, dObj) != null) {
+                return false;
+            }
+            //update the object of this auth
+            String keyId = UUID.randomUUID().toString();
+            String keyValue = RandomStringUtils.getRandomString(43);
+            auth.setKeyId(keyId);
+            auth.setKeyValue(keyValue);
+            auth.setObjId(dObj.getObjectID());
+            JSONArray objItem = (JSONArray) jcontent.get(authName);
+            objItem.add(auth);
+            jcontent.remove(authName);
+            jcontent.put(authName, objItem);
+        } else {
+            //create a new auth
+            String keyId = UUID.randomUUID().toString();
+            String keyValue = RandomStringUtils.getRandomString(43);
+            auth.setKeyId(keyId);
+            auth.setKeyValue(keyValue);
+            auth.setObjId(dObj.getObjectID());
+            JSONArray objItem = new JSONArray();
+            objItem.add(auth);
+            jcontent.put(authName, objItem);
         }
-        //Struct to JsonObject
-        //KeySet keySet = new KeySet(userName,key);
-        keySet.setKey(key);
-        keySet.setUsername(userName);
-        JSONObject obj = new JSONObject();
-        JSONDomain data = new JSONDomain();   // for convert
-        data.setResult(keySet);
-        obj.put(userName, data.getResult());
-        System.out.println(obj.toJSONString(JSONStyle.NO_COMPRESS));
         //File io
         try {
             File f = new File(filePath);
             if (!f.exists()) {
                 f.createNewFile();
             }
-            BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
-            bw.append(obj.toJSONString(JSONStyle.NO_COMPRESS) + "\r\n");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f, false));
+
+            bw.write(jcontent.toJSONString());
+            //bw.append(obj.toJSONString(JSONStyle.NO_COMPRESS) + "\r\n");
             bw.flush();
             bw.close();
             return true;
@@ -107,16 +138,4 @@ public class KMS {
         return false;
     }
 
-    public class JSONDomain {    // for convert struct <==> json
-
-        public Object result = new JSONObject();
-
-        public Object getResult() {
-            return result;
-        }
-
-        public void setResult(Object result) {
-            this.result = result;
-        }
-    }
 }
